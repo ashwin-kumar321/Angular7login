13969919e2dccf96afec4f2e02fd5f6b56e86691
ashwin <ash.kumar3211@gmail.com>
2020-01-13 17:34:56 +0530

    first commit

---
+++ angular7login/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './_services';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular7login';
+  currentUser: any;
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+}
+
+logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+}
+}
